ac56c6e29a27702e09d57e6f8b033051ff915cc1
LeemHyungJun <a01085357914@gmail.com>
2024-12-27 12:23:08 +0900

    fix challenge code more simple

--- assignment/Challenge.cpp
+++ assignment/Challenge.cpp
@@ -1,57 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int PartitionAscend(int board[], int low, int high)
+int Partition(int board[], int low, int high, int num)
 {
 	int pivot = board[high];
 	int i = low - 1;
 	for (int j = low; j < high; ++j)
 	{
-		if (board[j] < pivot)
+		if (num == 1)
 		{
-			i += 1;
-			swap(board[i], board[j]);
+			if (board[j] < pivot)
+			{
+				i += 1;
+				swap(board[i], board[j]);
+			}
 		}
-	}
-	swap(board[i + 1], board[high]);
-	return i + 1;	
-}
-
-int PartitionDescend(int board[], int low, int high)
-{
-	int pivot = board[high];
-	int i = low - 1;
-	for (int j = low; j < high; ++j)
-	{
-		if (board[j] > pivot)
+		else if (num == 2)
 		{
-			i += 1;
-			swap(board[i], board[j]);
+			if (board[j] > pivot)
+			{
+				i += 1;
+				swap(board[i], board[j]);
+			}
 		}
 	}
 	swap(board[i + 1], board[high]);
 	return i + 1;
 }
 
-void QuickSortA(int board[], int low, int high)
+void QuickSort(int board[], int low, int high, int num)
 {
 	if (low < high)
-	{		
-		int pos = PartitionAscend(board, low, high);		
+	{
+		int pos = Partition(board, low, high, num);
 
-		QuickSortA(board, low, pos - 1);
-		QuickSortA(board, pos + 1, high);
-	}
-}
-
-void QuickSortD(int board[], int low, int high)
-{
-	if (low < high)
-	{		
-		int pos = PartitionDescend(board, low, high);
-
-		QuickSortD(board, low, pos - 1);
-		QuickSortD(board, pos + 1, high);
+		QuickSort(board, low, pos - 1, num);
+		QuickSort(board, pos + 1, high, num);
 	}
 }
 
@@ -78,19 +62,15 @@ int main()
 	cout << "input 1(ascending order) or 2(descending order)\n";
 
 	cin >> num;
-	if (num == 1)
-	{
-		QuickSortA(board, 0, 9);
-		Print(board, 10);
-	}
-	else if(num == 2)
-	{
-		QuickSortD(board, 0, 9);
-		Print(board, 10);
-	}
-	else
+
+	while (num != 1 && num != 2)
 	{
 		cout << "input 1 or 2\n";
 		cin >> num;
 	}
+	
+	QuickSort(board, 0, 9, num);
+	
+
+	Print(board, 10);
 }
